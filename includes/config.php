9e4b20379fd007690a2256161a672878b4273916
Arshdeep Singh <arshdeepjhagra@gmail.com>
2025-10-21 12:26:19 +0400

    `Update BASE_URL constant in config.php`

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
 // Site Configuration
-define('BASE_URL', '/'); // Change this if the site is in a subdirectory
+define('BASE_URL', '/preview/999tra79c1a4e9b999qqqq/'); // Change this if the site is in a subdirectory
 
 // Function to get full URL
 function url($path = '')
